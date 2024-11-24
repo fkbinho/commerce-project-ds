0b907f798fb3896d5369a0b63bf3be16af34e960
fkbinho <sir_kleber@hotmail.com>
2024-11-23 23:00:37 -0300

    GET Products paged

--- src/main/java/com/fkbinho/commerce/Services/ProductService.java
+++ src/main/java/com/fkbinho/commerce/Services/ProductService.java
@@ -4,8 +4,12 @@ import com.fkbinho.commerce.Repositories.ProductRepository;
 import com.fkbinho.commerce.dto.ProductDTO;
 import com.fkbinho.commerce.entities.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class ProductService {
@@ -18,4 +22,11 @@ public class ProductService {
         Product product = productRepository.findById(id).get();
         return new ProductDTO(product);
     }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        Page<Product> result = productRepository.findAll(pageable);
+        return result.map(x -> new ProductDTO(x));
+    }
+
 }
