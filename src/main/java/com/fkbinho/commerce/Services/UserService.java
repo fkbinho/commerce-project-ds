3683129cf5d6c1a440a8424aae53369c9f243497
fkbinho <sir_kleber@hotmail.com>
2025-01-06 15:48:03 -0300

    UserDatailsService

---
+++ src/main/java/com/fkbinho/commerce/Services/UserService.java
@@ -0,0 +1,37 @@
+package com.fkbinho.commerce.Services;
+
+import com.fkbinho.commerce.Repositories.UserRepository;
+import com.fkbinho.commerce.entities.Role;
+import com.fkbinho.commerce.entities.User;
+import com.fkbinho.commerce.projections.UserDetailsProjection;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        List<UserDetailsProjection> result = repository.searchUserAndRolesByEmail(username);
+        if (result.size() == 0) {
+            throw new UsernameNotFoundException("User not found");
+        }
+
+        User user = new User();
+        user.setEmail(username);
+        user.setPassword(result.get(0).getPassword());
+        for (UserDetailsProjection projection : result) {
+            user.addRole(new Role(projection.getRoleId(), projection.getAuthority()));
+        }
+
+        return user;
+    }
+}
