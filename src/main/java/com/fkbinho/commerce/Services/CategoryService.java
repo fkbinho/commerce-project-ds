39222580efe211bc627d64bc978db8f99a51d23f
fkbinho <sir_kleber@hotmail.com>
2025-01-09 23:02:17 -0300

    GET categories

---
+++ src/main/java/com/fkbinho/commerce/Services/CategoryService.java
@@ -0,0 +1,23 @@
+package com.fkbinho.commerce.Services;
+
+import com.fkbinho.commerce.Repositories.CategoryRepository;
+import com.fkbinho.commerce.dto.CategoryDTO;
+import com.fkbinho.commerce.entities.Category;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository repository;
+
+    @Transactional(readOnly = true)
+    public List<CategoryDTO> findAll() {
+        List<Category> result = repository.findAll();
+        return result.stream().map(x -> new CategoryDTO(x)).toList();
+    }
+}
---
+++ src/main/java/com/fkbinho/commerce/controllers/CategoryController.java
@@ -0,0 +1,25 @@
+package com.fkbinho.commerce.controllers;
+
+import com.fkbinho.commerce.Services.CategoryService;
+import com.fkbinho.commerce.dto.CategoryDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService service;
+
+    @GetMapping
+    public ResponseEntity<List<CategoryDTO>> findAll() {
+        List<CategoryDTO> list = service.findAll();
+        return ResponseEntity.ok(list);
+    }
+}
