829b4a382f3efab321870f5d894ce333a70b0958
fkbinho <sir_kleber@hotmail.com>
2025-01-09 23:12:30 -0300

    Fix validation

--- src/main/java/com/fkbinho/commerce/dto/ProductDTO.java
+++ src/main/java/com/fkbinho/commerce/dto/ProductDTO.java
@@ -2,10 +2,7 @@ package com.fkbinho.commerce.dto;
 
 import com.fkbinho.commerce.entities.Category;
 import com.fkbinho.commerce.entities.Product;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,6 +18,7 @@ public class ProductDTO {
     @NotBlank(message = "Campo requerido.")
     private String description;
 
+    @NotNull(message = "Campo requerido")
     @Positive(message = "O preço deve ser positivo.")
     private Double price;
     private String imgUrl;
--- src/main/java/com/fkbinho/commerce/dto/ValidationError.java
+++ src/main/java/com/fkbinho/commerce/dto/ValidationError.java
@@ -16,7 +16,8 @@ public class ValidationError extends CustomError {
         return errors;
     }
 
-    public void addError(String fielName, String message) {
-        errors.add(new FieldMessage(fielName,message));
+    public void addError(String fieldName, String message) {
+        errors.removeIf(x -> x.getFieldName().equals(fieldName));
+        errors.add(new FieldMessage(fieldName,message));
     }
 }
